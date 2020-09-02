e001d145ab0cb4e1b9e256b20dcdd1828d9f6c26
dhenry21 <dalehenry21@gmail.com>
2020-09-02 13:46:13 -0700

    Added hand and interactions for L and R hand

---
+++ Assets/Scripts/HandAnimation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class HandAnimation : MonoBehaviour
+{
+    public Handness handness;
+    private string gripButtonName;
+
+
+    private Animator animator;
+
+    public void Awake()
+    {
+        if (handness == Handness.Right)
+        {
+            gripButtonName = "RightGrip";
+        }
+        else
+        {
+            gripButtonName = "LeftGrip";
+        }
+
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void Update()
+    {
+
+        float gripValue = Input.GetAxis(gripButtonName);
+
+        if (gripValue > 0.8)
+        {
+            animator.SetBool("isGrabbing", true);
+
+        }
+
+        else if (gripValue < 0.2)
+        {
+            animator.SetBool("isGrabbing", false);
+
+        }
+
+    }
+}
